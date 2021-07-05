83835adf88d9ad9a47e32871efbf9c764b3b0726
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2021-07-05 16:09:07 -0300

    Projeto do bootcamp DIO

---
+++ src/main/java/com/controleDeAcesso/controller/JornadaTrabalhoController.java
@@ -0,0 +1,47 @@
+package com.controleDeAcesso.controller;
+
+import com.controleDeAcesso.model.JornadaTrabalho;
+import com.controleDeAcesso.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaById(@PathVariable("idJornada") long idJornada) throws Exception {
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new Exception("Jornada não encontrada.")));
+    }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> deleteJornadaById(@PathVariable("idJornada") long idJornada) throws Exception {
+        try {
+            jornadaService.deleteJornada(idJornada);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
+    }
+}
---
+++ src/main/java/com/controleDeAcesso/service/JornadaService.java
@@ -0,0 +1,43 @@
+package com.controleDeAcesso.service;
+
+import com.controleDeAcesso.model.JornadaTrabalho;
+import com.controleDeAcesso.repository.JornadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JornadaService {
+
+
+    JornadaRepository jornadaRepository;
+
+    @Autowired
+    public JornadaService(JornadaRepository jornadaRepository) {
+        this.jornadaRepository = jornadaRepository;
+    }
+
+    public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+
+    public List<JornadaTrabalho> findAll() {
+        return jornadaRepository.findAll();
+    }
+
+
+    public Optional<JornadaTrabalho> getById(long idJornada) {
+        return jornadaRepository.findById(idJornada);
+    }
+
+    public JornadaTrabalho updateJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public void deleteJornada(long idJornada) {
+        jornadaRepository.deleteById(idJornada);
+    }
+}
